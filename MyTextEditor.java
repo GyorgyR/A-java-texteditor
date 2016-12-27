4c36e4bdfa2b450f636f2542b929c493821b7817
Gyorgy Rethy <gyorgy.rethy@student.manchester.ac.uk>
2016-12-27 17:28:01 +0000

    init

---
+++ MyTextEditor.java
@@ -0,0 +1,20 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*******************************************************************************
+Author: Gyorgy Rethy
+Date: 27/12/2016
+--------------------------------------------------------------------------------
+This a simple java text editor so I will not forget everything that I learned.
+*******************************************************************************/
+public class MyTextEditor extends JFrame implements ActionListener
+{
+	//This array holds everything that the user writes
+	private String[] input;
+	
+}
